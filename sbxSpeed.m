14fb7188523b57494e3c59996347ca9ca59399a7
Arthur Sugden <Arthur Sugden>
2016-06-16 11:58:00 -0400

    Convert quadrature encoder data to running speed for 3D printed wheels.

---
+++ sbxSpeed.m
@@ -0,0 +1,31 @@
+function speed = sbxSpeed(mouse, date, run)
+%SBXSPEED Return the speed of a mouse running on a 3d printed wheel from
+% date from the quadrature encoder
+
+    dirs = sbxDir(mouse, date, run);
+    dirs = dirs.runs{1};
+    
+    inf = sbxLoad(mouse, date, run, 'info');
+    framerate = 30.98;
+    if inf.scanmode == 0, framerate = framerate/2; end
+
+    quadfile = load(dirs.quad);
+    running = quadfile.quad_data;
+
+    wheel_diameter = 14; % in cm
+    wheel_tabs = 44; 
+    wheel_circumference = wheel_diameter*pi;
+    step_size = wheel_circumference/(wheel_tabs*2);
+
+    instantaneous_speed = zeros(length(running), 1);
+    if ~isempty(instantaneous_speed)
+        size(diff(running))
+        size(instantaneous_speed)
+        instantaneous_speed(2:end) = diff(running);
+        instantaneous_speed(2) = 0;
+        instantaneous_speed = instantaneous_speed*step_size*framerate;
+    end
+    
+    speed = conv(instantaneous_speed, ones(ceil(framerate), 1)/ceil(framerate), 'same');
+end
+
