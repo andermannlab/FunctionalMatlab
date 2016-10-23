b4c35a32cc532805b17917dad3d39a261db26a4c
Arthur Sugden <Arthur Sugden>
2016-10-23 16:15:03 -0400

    Now sorts the output

--- sbxRuns.m
+++ sbxRuns.m
@@ -2,8 +2,9 @@ function runs = sbxRuns(mouse, date)
 %SBXRUNS List all runs in a folder
     
     % Initialize the base directory and scan directory
-    base = 'D:\twophoton_data\2photon\scan\';
-    searchdir = sprintf('%s%s\\%s_%s\\', base, mouse, date, mouse);
+    % Get the base directory from sbxDir.
+    
+    searchdir = sbxMouseDateDir(mouse, date);
     matchstr = sprintf('%s_%s_run', date, mouse);
     runs = [];
     
@@ -13,10 +14,17 @@ function runs = sbxRuns(mouse, date)
         if fs(i).isdir
             if length(fs(i).name) > length(matchstr)
                 if strcmp(fs(i).name(1:length(matchstr)), matchstr)
-                    runs = [runs str2num(fs(i).name(length(matchstr) + 1:end))];
+                    runnum = [];
+                    j = length(matchstr) + 1;
+                    while j <= length(fs(i).name) && isstrprop(fs(i).name(j), 'digit')
+                        runnum = [runnum fs(i).name(j)];
+                        j = j + 1;
+                    end
+                    runs = [runs str2num(runnum)];
                 end
             end
         end
     end
+    runs = sort(runs);
 end
 
