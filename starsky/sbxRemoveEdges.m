a152ebc739890de3752748b3892cb98088ba0c23
Arthur Sugden <Arthur Sugden>
2016-10-23 16:15:43 -0400

    Starsky-specific functions.

---
+++ starsky/sbxRemoveEdges.m
@@ -0,0 +1,10 @@
+function out = sbxRemoveEdges()
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+
+    % Left, right, top bottom
+    out = [50, 70, 5, 5];
+end
+
