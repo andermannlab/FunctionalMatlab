ed049eca93cc0a08159642e6470f6e563da359aa
Arthur Sugden <Arthur Sugden>
2016-12-06 10:02:32 -0500

    Formatting changes

--- sbxScanbase.m
+++ sbxScanbase.m
@@ -2,13 +2,13 @@ function base = sbxScanbase()
 %SBXSCANBASE Hard codes directories
 
     % Set base path depending on server
-    if strcmp(hostname,'Megatron')
+    if strcmp(hostname, 'Megatron')
         base = 'D:\twophoton_data\2photon\scan\';
-    elseif strcmp(hostname,'Atlas')
+    elseif strcmp(hostname, 'Atlas')
         base = 'E:\twophoton_data\2photon\raw\';
-    elseif strcmp(hostname,'BeastMode')
+    elseif strcmp(hostname, 'BeastMode')
         base = 'S:\twophoton_data\2photon\scan\';
-    elseif strcmp(hostname,'Sweetness')
+    elseif strcmp(hostname, 'Sweetness')
         base = 'D:\2p_data\scan\';
     end
 end
